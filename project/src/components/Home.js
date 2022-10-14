2790ac694271b8eca11c92a848e30e196b272b66
Pratik-lab-max <104593495+Pratik-lab-max@users.noreply.github.com>
2022-10-14 22:49:24 +0530

    Update Home.js

--- project/src/components/Home.js
+++ project/src/components/Home.js
@@ -5,6 +5,21 @@ const home = () => {
     <div>
       <h1>Pratik</h1>
       <p>Learn MERN stack developer at Lnb</p>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="https://cdn.educba.com/academy/wp-content/uploads/2019/11/full-stack-web-developer.png" alt="First slide" />
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
     </div>
   )
 }
